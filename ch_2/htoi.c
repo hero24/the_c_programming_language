a631280197e8e654cc9dce418efedcdeca82b42b
hero24 <hero24@interia.pl>
2016-12-06 00:40:59 +0000

    Update htoi.c

--- ch_2/htoi.c
+++ ch_2/htoi.c
@@ -1,32 +1,35 @@
 #include <stdio.h>
 enum TYPES {NONE=0,DIGIT,LOWER,UPPER};
-
-int is_digit(char i)
+int get_type(char c)
 {
-    return i >=  '0' && i <= '9' ? DIGIT:NONE;
-}
-int is_lower(char i)
-{
-    return i >= 'a' && i <= 'f' ? LOWER:NONE;
-}
-int is_upper(char i)
-{
-    return i >= 'A' && i <= 'F' ? UPPER:NONE;
+    int type = NONE;
+    if(c >=  '0' && c <= '9') 
+        type = DIGIT;
+    else if(c >= 'a' && c <= 'f')
+        type = LOWER;
+    else if(c >= 'A' && c <= 'F')
+        type = UPPER;
+    return type;
 }
 int htoi(char s[])
 {
-    int i,n,type;
-    n = 0;
-    for(i = 0;(type = is_digit(s[i])) || (type = is_lower(s[i])) || (type = is_upper(s[i]));++i){
+    int i,value,type;
+    value = 0;
+    if(s[0] == '0' && (s[1] == 'x' || s[1] == 'X'))
+        i = 2;
+    else
+        i = 0;
+    for(;(type = get_type(s[i]));++i){
           switch(type){
                case DIGIT:
-                    n = 16 * n + s[i] - '0';
+                    value = 16 * n + s[i] - '0';
                     break;
                case LOWER:
-                    n = 16 * n + ((s[i] - 'a')+10);
+                    value = 16 * n + ((s[i] - 'a')+10);
                     break;
                case UPPER:
-                    n = 16 * n + ((s[i] - 'A')+10);
+                    value = 16 * n + ((s[i] - 'A')+10);
+                    break;
           }
           
     }
