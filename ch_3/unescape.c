51a5796b54a6142b1ffe4072749c59c8fc879935
hero24 <hero24@interia.pl>
2016-12-10 14:59:25 +0000

    Create unescape.c

---
+++ ch_3/unescape.c
@@ -0,0 +1,49 @@
+void unescape(char s[], char t[])
+{
+     int i,k;
+     i = k = 0;
+     int escaped = 0;
+     while(s[i] != '\0'){
+                switch(s[i]){
+                             case '\\':
+                                  i++;
+                                  escaped = 1;
+                                  break;
+                             case 'b':
+                                  t[k++] = (escaped) ? '\b':s[i];
+                                  escaped = 0;
+                                  i++;
+                                  break;
+                             case 'f':
+                                  t[k++] = (escaped) ? '\f':s[i];
+                                  escaped = 0;
+                                  i++;
+                                  break;
+                             case 'n':
+                                  t[k++] = (escaped) ? '\n':s[i];
+                                  escaped = 0;
+                                  i++;
+                                  break;
+                             case 'r':
+                                  t[k++] = (escaped) ? '\r':s[i];
+                                  escaped = 0;
+                                  i++;
+                                  break;
+                             case 't':                                  
+                                  t[k++] = (escaped) ? '\t':s[i];
+                                  escaped = 0;
+                                  i++;
+                                  break;
+                             case 'v':                                  
+                                  t[k++] = (escaped) ? '\v':s[i];
+                                  escaped = 0;
+                                  i++;
+                                  break;
+                             default:
+                                  t[k++] = s[i++];
+                                  break;                        
+                }
+     }
+     t[k] = '\0';
+     
+}
