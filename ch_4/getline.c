00a0c94f89b5ddfd96fb950b54f50083183af9f7
hero24 <hero24@interia.pl>
2016-12-15 18:25:23 +0000

    Create getline.c

---
+++ ch_4/getline.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/* 
+  get line from input to s, return length
+  no string length validation.
+*/
+int getline(char string[])
+{
+    int c,i = 0;
+    while((c=getchar()) != EOF && c != '\n'){
+                        string[i++] = c;
+    }
+    if(c == '\n'){
+         string[i++] = c;
+    }
+    s[i] = '\0';
+    return i
+    
+}
+/* 
+  get line from input to s, return length
+  string length validation - make sure you do not write to place
+  in the array that does not exist.
+*/
+int getline(char string[],int limit)
+{
+    int c,i = 0;
+    while(--limit > 0 && (c=getchar()) != EOF && c != '\n'){
+                        string[i++] = c;
+    }
+    if(c == '\n'){
+         string[i++] = c;
+    }
+    s[i] = '\0';
+    return i
+    
+}
